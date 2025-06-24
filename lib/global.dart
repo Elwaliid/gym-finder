8ee7bcab110b28eceb3f2d26d92ffdfe5aafa9be
Elwaliid <168366600+Elwaliid@users.noreply.github.com>
2025-06-24 15:42:03 +0100

    Delete lib directory

--- lib/global.dart
+++
@@ -1,8 +0,0 @@
-import 'package:gym_finder/model/posting_model.dart';
-import 'package:gym_finder/view_model/user_view_model.dart';
-import 'package:gym_finder/view_model/posting_view_model.dart';
-
-PostingModel postingModel = PostingModel();
-
-UserViewModel userViewModel = UserViewModel();
-PostingViewModel postingViewModel = PostingViewModel();
--- lib/model/review_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:gym_finder/model/contact_model.dart';
-
-class ReviewModel {
-  ContactModel? contact;
-  String? text;
-  double? rating;
-  DateTime? dateTime;
-  ReviewModel();
-}
